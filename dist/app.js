172a8530f36ac0e70f9a92c3ea011d8ebca9259f
gfreedland <gfreedland@mail.sfsu.edu>
2020-04-26 09:15:53 -0700

    form mockup added

--- dist/app.js
+++ dist/app.js
@@ -1,2 +1,14 @@
 "use strict";
-console.log('hi');
+class ProjectInput {
+    constructor() {
+        this.templateElement = document.getElementById('project-input'); // Gives access to template with conent
+        this.hostElement = document.getElementById('app'); // Holds reference to template content
+        const importedNode = document.importNode(this.templateElement.content, true);
+        this.element = importedNode.firstElementChild;
+        this.attach();
+    }
+    attach() {
+        this.hostElement.insertAdjacentElement('afterbegin', this.element);
+    }
+}
+const prjInput = new ProjectInput();
