232a6f612567b3dea367b51807730034f365828f
gfreedland <gfreedland@mail.sfsu.edu>
2020-05-14 18:24:17 -0700

    default exports component

--- src/components/project-input.ts
+++ src/components/project-input.ts
@@ -1,10 +1,11 @@
-import { Validatable, validate } from '../util/validation.js';
-import { Component } from './base-component.js';
-import { autobind } from '../decorators/autobind.js';
+// import { Validatable, validate } from '../util/validation.js';
+import * as Validation from '../util/validation.js';
+import Cmp from './base-component.js';
+import { autobind as Autobind } from '../decorators/autobind.js';
 import { projectState } from '../state/project-state.js';
 
 // ProjectInput Class
-export class ProjectInput extends Component<HTMLDivElement, HTMLFormElement> {
+export class ProjectInput extends Cmp<HTMLDivElement, HTMLFormElement> {
     titleInputElement: HTMLInputElement;
     descriptionInputElement: HTMLInputElement;
     peopleInputElement: HTMLInputElement;
@@ -29,16 +30,16 @@ export class ProjectInput extends Component<HTMLDivElement, HTMLFormElement> {
         const enteredDescription = this.descriptionInputElement.value;
         const enteredPeople = this.peopleInputElement.value;
 
-        const titleValidatable: Validatable = {
+        const titleValidatable: Validation.Validatable = {
             value: enteredTitle,
             required: true
         };
-        const descriptionValidatable: Validatable = {
+        const descriptionValidatable: Validation.Validatable = {
             value: enteredDescription,
             required: true,
             minLength: 5
         };
-        const peopleValidatable: Validatable = {
+        const peopleValidatable: Validation.Validatable = {
             value: +enteredPeople,
             required: true,
             min: 1,
@@ -49,9 +50,9 @@ export class ProjectInput extends Component<HTMLDivElement, HTMLFormElement> {
             // enteredTitle.trim().length === 0 || 
             // enteredDescription.trim().length === 0 || 
             // enteredPeople.trim().length === 0
-            !validate(titleValidatable) ||
-            !validate(descriptionValidatable) ||
-            !validate(peopleValidatable) 
+            !Validation.validate(titleValidatable) ||
+            !Validation.validate(descriptionValidatable) ||
+            !Validation.validate(peopleValidatable) 
         ) {
             alert('Invalid input, please try again!');
             return;
@@ -67,7 +68,7 @@ export class ProjectInput extends Component<HTMLDivElement, HTMLFormElement> {
         this.peopleInputElement.value = '';
     }
 
-    @autobind
+    @Autobind
     private submitHandler(event: Event) {
         event.preventDefault(); // Prevents HTTP Request from being sent and page reload
         // console.log(this.titleInputElement.value);
